d5fbf24507d3dbf25b376410d9fba0e962198d22
Over1Cloud <48094576+Over1Cloud@users.noreply.github.com>
2024-06-11 19:15:19 +0300

    Update script.js
    Возврат к первой версии

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-// Переменная-флаг, чтобы отслеживать, было ли уже отображено уведомление
-let notificationShown = false;
-
 // Функция для загрузки JSON из URL
 async function fetchJSON(url) {
-  if (!notificationShown) {
-    // Показываем уведомление о запуске загрузки JSON
-    displayMessage("Загрузка данных...");
-    notificationShown = true; // Устанавливаем флаг в true, чтобы уведомление отображалось только один раз
-  }
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Ошибка загрузки данных: " + error.message);
-  }
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 // Функция для проверки совпадения вопроса
@@ -29,60 +16,64 @@ function checkQuestion(text, answers) {
 }
 
 // Функция для выполнения действий на основе найденного ответа
-async function processAnswer(answer) {
-  if (!answer) {
-    throw new Error("Ответ не найден в базе данных");
-  }
-
-  const answerElement = document.evaluate(`//*[contains(text(), "${answer}")]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  if (!answerElement) {
-    throw new Error("Ответ не найден на сайте");
-  }
-
-  const inputElement = answerElement.closest('.choice-view__choice-container').querySelector('input[type="radio"]');
-  if (!inputElement) {
-    throw new Error("Не удалось найти соответствующий input элемент");
-  }
-
-  inputElement.click();
-  inputElement.setAttribute('aria-checked', 'true');
+function processAnswer(answer) {
+  return new Promise((resolve, reject) => {
+    if (answer) {
+      const answerElement = document.evaluate(`//*[contains(text(), "${answer}")]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (answerElement) {
+        const inputElement = answerElement.closest('.choice-view__choice-container').querySelector('input[type="radio"]');
+        if (inputElement) {
+          inputElement.click();
+          inputElement.setAttribute('aria-checked', 'true');
+          displayMessage("Ответ найден", "Ответ найден на сайте");
+          resolve();
+        } else {
+          reject("Не удалось найти соответствующий input элемент");
+        }
+      } else {
+        reject("Ответ не найден на сайте");
+      }
+    } else {
+      reject("Ответ не найден в базе данных");
+    }
+  });
 }
 
-// Функция для отображения уведомления на странице
-function displayMessage(status) {
+// Функция для отображения сообщения на странице
+function displayMessage(questionStatus, answerStatus) {
   const messageContainer = document.createElement('div');
-  messageContainer.id = 'notification';
   messageContainer.style.position = 'fixed';
-  messageContainer.style.top = '20px';
-  messageContainer.style.left = '50%';
-  messageContainer.style.transform = 'translateX(-50%)';
-  messageContainer.style.padding = '15px';
-  messageContainer.style.backgroundColor = '#000';
-  messageContainer.style.color = '#fff';
-  messageContainer.style.borderRadius = '10px';
-  messageContainer.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+  messageContainer.style.top = '-9999px';
+  messageContainer.style.left = '10px';
+  messageContainer.style.padding = '10px';
+  messageContainer.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+  messageContainer.style.border = '1px solid #ccc';
   messageContainer.style.zIndex = '9999';
-  messageContainer.style.fontFamily = '-apple-system, BlinkMacSystemFont, sans-serif';
-  messageContainer.style.fontSize = '16px';
-  messageContainer.textContent = status;
+  messageContainer.innerHTML = `
+    <p>${questionStatus}</p>
+    <p>${answerStatus}</p>
+  `;
   document.body.appendChild(messageContainer);
-  
-  // Удаляем уведомление через 2 секунды
   setTimeout(() => {
     messageContainer.remove();
-  }, 2000);
+  }, 3000);
 }
 
 // Функция для выполнения парсинга и действий
 async function parseAndProcess() {
+  const data = await fetchJSON('https://raw.githubusercontent.com/Over1Cloud/rostrans/main/answers.json');
+  const textOnPage = document.body.innerText;
+  const answer = checkQuestion(textOnPage, data);
+
   try {
-    const data = await fetchJSON('https://raw.githubusercontent.com/Over1Cloud/rostrans/main/answers.json');
-    const textOnPage = document.body.innerText;
-    const answer = checkQuestion(textOnPage, data);
     await processAnswer(answer);
   } catch (error) {
-    displayMessage("Ошибка: " + error.message);
+    displayMessage("Ошибка", error);
   }
+
+  // Рекурсивный вызов функции для ожидания следующего вопроса
+  await new Promise(resolve => setTimeout(resolve, 0));
+  parseAndProcess();
 }
 
 // Вызов функции для парсинга и выполнения действий
